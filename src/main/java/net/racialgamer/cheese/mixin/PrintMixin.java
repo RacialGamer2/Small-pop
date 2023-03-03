88ac7d35b78f5747dd1dd9e3803bb8ca54b70a04
RacialGamer <121128992+RacialGamer@users.noreply.github.com>
2023-03-03 16:20:21 +0100

    Add files via upload

---
+++ src/main/java/net/racialgamer/cheese/mixin/PrintMixin.java
@@ -0,0 +1,16 @@
+package net.racialgamer.cheese.mixin;
+
+import net.racialgamer.cheese.Smallpop;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class PrintMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		Smallpop.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
